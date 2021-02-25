a6866c1e2d216ae9f26d0283210e4b4082dd67d4
roomasa <75815560+roomasa@users.noreply.github.com>
2021-02-25 08:12:07 -0600

    flask app

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
 from flask import Flask
-#following code creates a new Flask instance call app
+import datetime as dt
+import numpy as np
+import pandas as pd
+#dependencies for SQLAlchemy
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+#import dependencies for Flask
+from flask import Flask, jsonify
+#create the database engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+#reflect the database into our classes.
+Base = automap_base()
+#Add the following code to reflect the database
+Base.prepare(engine, reflect=True)
+#save our references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+#create a session link from Python to our database with the following code
+session = Session(engine)
+#define our Flask app, add the following line of code. This will create a Flask application called "app."
 app = Flask(__name__)
-#First, we need to define the starting point, also known as the root. To do this, we'll use the function @app.route('/'). Add this to your code now.
-@app.route('/')
-def hello_world():
-    return 'Hello world'
-def surfs_up():
-    return "Surf's up"
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
